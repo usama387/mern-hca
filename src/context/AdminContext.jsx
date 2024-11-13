c79f329b74bc935b81f0c51147a4291ff91c3103
usama387 <128306049+usama387@users.noreply.github.com>
2024-11-13 13:16:26 +0500

    Added token states and backend url when used in Login Page the token after login gets saved in this state of AdminContext

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,10 +1,19 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
-  // this object contains data to be accessed and passing it as prop in provider
-  const value = {};
+  // creating a state to save admin token
+  const [atoken, setAToken] = useState("");
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  // this object contains data to be accessed anywhere in app and passing it as prop in provider
+  const value = {
+    atoken,
+    setAToken,
+    backendUrl,
+  };
 
   return (
     <AdminContext.Provider value={value}>
