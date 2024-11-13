abfc535b37376200b75cf22173b7eb28114fa103
usama387 <128306049+usama387@users.noreply.github.com>
2024-11-13 13:32:40 +0500

    Added ternary operator once admin is logged in the login page becomes invisible

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { useContext } from "react";
 import Login from "./pages/Login";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AdminContext } from "./context/AdminContext";
 
 const App = () => {
-  return <div>
-    <Login />
-    <ToastContainer />
-  </div>;
+  const { atoken } = useContext(AdminContext);
+
+  // where there is token Login Page is not visible means we have an admin logged in
+  return atoken ? (
+    <div>
+      <ToastContainer />
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
+  );
 };
 
 export default App;
