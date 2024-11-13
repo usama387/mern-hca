ffcddd760fb334321a95c1fba527fcbf733d41f2
usama387 <128306049+usama387@users.noreply.github.com>
2024-11-13 13:14:51 +0500

    added react-toastify in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Login from "./pages/Login";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return <div>
     <Login />
+    <ToastContainer />
   </div>;
 };
 
