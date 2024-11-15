61cdc87c3df565205a12cc1599942cc591b5c70d
usama387 <128306049+usama387@users.noreply.github.com>
2024-11-15 20:27:43 +0500

    Added a state variable for doctors data and fetched api that returns all doctors and updates the state with setDoctors and passing in data also this api requires atoken in headers meaning only admin can use this api

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,4 +1,6 @@
 import { createContext, useState } from "react";
+import axios from "axios";
+import toast from "react-toastify";
 
 export const AdminContext = createContext();
 
@@ -8,13 +10,38 @@ const AdminContextProvider = (props) => {
     localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
   );
 
+  // backend url address
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  // managing state for doctors data when api returns response doctors state is updated with the data
+  const [doctors, setDoctors] = useState([]);
+
+  const getAllDoctors = async () => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/all-doctors",
+        {},
+        { headers: atoken }
+      );
+
+      if (data.success) {
+        setDoctors(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
 
   // this object contains data to be accessed anywhere in app and passing it as prop in provider
   const value = {
     atoken,
     setAToken,
     backendUrl,
+    doctors,
+    getAllDoctors,
   };
 
   return (
