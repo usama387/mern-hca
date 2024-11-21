77b419c52c8344a223b4cda23ef85d3b9827d94c
usama387 <128306049+usama387@users.noreply.github.com>
2024-11-21 21:00:50 +0500

    Added two functions one is for formatting date and other one is for calculating age using dob as a parameter

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,8 +3,45 @@ import { createContext } from "react";
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
+  // function to calculate patient age using its dob
+  const calculateAge = (dob) => {
+    const today = new Date();
+    const birthdate = new Date(dob);
+
+    let age = today.getFullYear() - birthdate.getFullYear();
+
+    return age;
+  };
+
+  // slot date format function
+  const slotDateFormat = (slotDate) => {
+    const dateArray = slotDate.split("_");
+    const months = [
+      "",
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    return `${dateArray[0]} ${months[Number(dateArray[1])]} ${dateArray[2]}`;
+  };
+
+  const currency = "PKR";
+
   // this object contains data to be accessed and passing it as prop in provider
-  const value = {};
+  const value = {
+    calculateAge,
+    slotDateFormat,
+    currency,
+  };
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
